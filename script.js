585a732dcd28b6d75b24879af0bdfa9ca4cb3f75
dru hippo <119908883+druhippo@users.noreply.github.com>
2025-01-18 21:36:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const unicorn = document.getElementById('unicorn');
+const feedButton = document.getElementById('feedButton');
+const waterButton = document.getElementById('waterButton');
+const petButton = document.getElementById('petButton');
+
+// Function to play a sound
+function playSound(soundFile) {
+    const audio = new Audio(soundFile);
+    audio.play();
+}
+
+// Play neigh sound when page loads
+playSound('neigh.mp3');
+
+feedButton.addEventListener('click', () => {
+    console.log('Feeding the unicorn!');
+    unicorn.src = 'unicorn3.jpg';
+    playSound('crunch.mp3');
+    setTimeout(() => {
+        unicorn.src = 'unicorn1.jpg';
+         playSound('magic.mp3');
+    }, 5000);
+});
+
+waterButton.addEventListener('click', () => {
+    console.log('Watering the unicorn!');
+     unicorn.src = 'unicorn2.jpg';
+    playSound('drink.mp3');
+     setTimeout(() => {
+         unicorn.src = 'unicorn1.jpg';
+          playSound('magic.mp3');
+    }, 10000);
+});
+petButton.addEventListener('click', () => {
+    console.log('Petting the unicorn!');
+    unicorn.src = 'pet.jpg';
+    playSound('iloveyou.mp3');
+     setTimeout(() => {
+         unicorn.src = 'unicorn1.jpg';
+         playSound('magic.mp3');
+     }, 3000);
+ });
